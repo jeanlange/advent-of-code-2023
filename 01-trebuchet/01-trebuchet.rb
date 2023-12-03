eca339e5aa838cd02845566866e1b083f8b1143c
Jean Lange <jean.f.lange@gmail.com>
2023-12-03 00:28:55 -0500

    part two, fast and dirty

--- 01-trebuchet/01-trebuchet.rb
+++ 01-trebuchet/01-trebuchet.rb
@@ -1,4 +1,5 @@
 lines = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+pt2_lines = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
 
 
 
@@ -7,12 +8,14 @@ nums = (0..9).to_a.map{|one| one.to_s}
 
 sum = 0
 
+
 File.open('01-input.txt').each do |line|
+# pt2_lines.each do |line|
+    line = line.gsub("one", "o1e").gsub("two", "t2o").gsub("three", "t3e").gsub("four", "f4r").gsub("five", "f5e").gsub("six", "s6x").gsub("seven", "s7n").gsub("eight", "e8t").gsub("nine", "n9e")
     first = line.match(/\d/) # first digit
     last  = line.reverse.match(/\d/) # last digit
 
     num = (first.to_s + last.to_s).to_i
-    puts num
     sum += num
 end
 
